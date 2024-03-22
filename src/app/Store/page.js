1283b1086646f33deefcaab6095ca6ad6baae289
gusdn010158 <gusdn010158@gmail.com>
2024-03-22 19:50:34 +0900

    header

--- src/app/Store/page.js
+++ src/app/Store/page.js
@@ -5,16 +5,67 @@ import Slickst from "./Slickst";
 import styles from "../Store/store.module.css";
 function page() {
   const [T, setT] = useState([]);
-
+  const [d, setd] = useState([]);
+  const [a, seta] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:3001/store").then((res) => {
       setT(res.data);
     });
   }, []);
+  useEffect(() => {
+    axios.get("http://localhost:3001/store2").then((res) => {
+      seta(res.data);
+    });
+  }, []);
+  useEffect(() => {
+    axios.get("http://localhost:3001/mainitem").then((res) => {
+      setd(res.data);
+    });
+  });
   return (
     <div className={styles.store}>
       <Slickst />
       <div className={styles.storein}>
+        <div className={styles.stitem}>
+          {d.map((m) => (
+            <div className={styles.sti}>
+              <img className={styles.sticon} src={m.img} />
+              <div>{m.name}</div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <div>
+            <h3>오늘의 딜</h3>
+            <div>더보기</div>
+          </div>
+          <div className={styles.storeflex}>
+            {a.map((m) => (
+              <div className={styles.storeitem}>
+                <div className={styles.storeimgtem}>
+                  <img className={styles.storeimg} src={m.img} />
+                </div>
+
+                <div>
+                  <div>{m.name}</div>
+                  <div>{m.text}</div>
+                  <div>{m.price}원</div>
+                  <div className={styles.emr}>특가</div>
+                  <div className={styles.gkf}>할인 쿠폰</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <h3>카테고리</h3>
+        <div className={styles.stitem}>
+          {d.map((m) => (
+            <div className={styles.sti}>
+              <img className={styles.sticon} src={m.img} />
+              <div>{m.name}</div>
+            </div>
+          ))}
+        </div>
         <div className={styles.storein2}>
           <div className={styles.storein3}>
             <div>
@@ -27,12 +78,16 @@ function page() {
         <div className={styles.storeflex}>
           {T.map((m) => (
             <div className={styles.storeitem}>
-              <img className={styles.storeimg} src={m.img} />
-              <div>{m.name}</div>
-              <div>{m.text}</div>
-              <div>{m.price}원</div>
-              <div>특가</div>
-              <div>할인 쿠폰</div>
+              <div className={styles.storeimgtem}>
+                <img className={styles.storeimg} src={m.img} />
+              </div>
+              <div>
+                <div>{m.name}</div>
+                <div>{m.text}</div>
+                <div>{m.price}원</div>
+                <div className={styles.emr}>특가</div>
+                <div className={styles.gkf}>할인 쿠폰</div>
+              </div>
             </div>
           ))}
         </div>
