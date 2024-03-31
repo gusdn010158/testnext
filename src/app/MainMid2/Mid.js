716429a5c9af8803bd35ef258d66dc069cd818e4
gusdn010158 <gusdn010158@gmail.com>
2024-03-31 13:24:16 +0900

    feat

--- src/app/MainMid2/Mid.js
+++ src/app/MainMid2/Mid.js
@@ -1,32 +1,8 @@
 import styles from "./Mid.module.css";
 import React from "react";
 
-import "slick-carousel/slick/slick.css";
-
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import api from "@/services/api";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-const CustomNextArrow = ({ className, onClick }) => (
-  <div className={className} onClick={onClick}>
-    <AiOutlineRight />
-  </div>
-);
 
-const CustomPrevArrow = ({ className, onClick }) => (
-  <div className={className} onClick={onClick}>
-    <AiOutlineLeft />
-  </div>
-);
-
-var settings = {
-  arrows: true,
-  nextArrow: <CustomNextArrow className="slick-next" />,
-  prevArrow: <CustomPrevArrow className="slick-prev" />,
-  slidesToScroll: 4,
-  slidesToShow: 4,
-  speed: 500,
-};
 function Mid() {
   const url = "http://localhost:3001/MidTwo";
 
@@ -38,13 +14,12 @@ function Mid() {
         <div>더보기</div>
       </div>
       <div className={styles.ctitem}>
-        <Slider {...settings}>
-          {data.map((m, id) => (
-            <div className={styles.ctitem1} key={id}>
-              <img alt="12" src={m.img} />
-            </div>
-          ))}
-        </Slider>
+        {data.map((m) => (
+          <div className={styles.Miditem}>
+            <img className={styles.Midimg} alt="12" src={m.img} />
+            <div>{m.title}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
