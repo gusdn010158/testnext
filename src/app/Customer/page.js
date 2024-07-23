c7e8d46752a8c718f461cdf4889bb959a5390dc8
gusdn010158 <gusdn010158@gmail.com>
2024-07-23 23:09:28 +0900

    error back

--- src/app/Customer/page.js
+++ src/app/Customer/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import api from "@/services/api";
 import styled from "styled-components";
 import { BiChevronDown } from "react-icons/bi";
@@ -7,18 +7,20 @@ import { CgSearch } from "react-icons/cg";
 import { Link } from "react-router-dom";
 import { BiChevronUp } from "react-icons/bi";
 import { FiPhone } from "react-icons/fi";
+
 function Customer() {
   const url = "http://localhost:3001/Customer";
-
   const data = api(url);
   const durl = "http://localhost:3001/Custom";
-
   const ddata = api(durl);
   const [open, setOpen] = useState(null); // 열린 아이템의 id를 저장할 state
+
+  const itemRefs = useRef([]); // useRef를 사용하여 참조 배열 생성
 
   const handleOpen = (id) => {
     setOpen(open === id ? null : id); // 클릭한 아이템의 id가 이미 열려있으면 닫고, 아니면 열기
   };
+
   return (
     <Custom>
       <Ctop>
@@ -28,35 +30,29 @@ function Customer() {
             <Cspan>Q</Cspan>배송은 얼마나 걸리나요?
           </div>
           <div>
-            {" "}
             <Cspan>Q</Cspan>주문 취소는 어떻게 하나요?
           </div>
           <div>
-            {" "}
             <Cspan>Q</Cspan>제품의 자세한 정보를 알고 싶어요.
           </div>
           <div>
-            {" "}
             <Cspan>Q</Cspan>제품이 불량일 때는?
           </div>
           <div>
-            {" "}
             <Cspan>Q</Cspan>카카오 계정으로 로그인하면 이미 가입되었다고 합니다.
           </div>
         </Ctoplf>
         <Ctoprt>
           <h3 className="font-noto_sans_kr">
-            <Ctoprtspan>고객센터</Ctoprtspan> 09:00 ~ 18:00{" "}
+            <Ctoprtspan>고객센터</Ctoprtspan> 09:00 ~ 18:00
           </h3>
           <li>평일: 전체 문의 상담</li>
           <li>
-            토요일, 공휴일: 오늘의집 직접배송, 이사/시공/제품설치 문의 상담{" "}
+            토요일, 공휴일: 오늘의집 직접배송, 이사/시공/제품설치 문의 상담
           </li>
           <li>일요일: 휴무</li>
           <h3>
-            {" "}
-            <FiPhone />
-            1670-0876
+            <FiPhone /> 1670-0876
           </h3>
           <Btndiv>
             <Topbtn>1:1 카톡 상담하기</Topbtn>
@@ -69,8 +65,14 @@ function Customer() {
       </Ctop>
       <Cbt>
         <CbtTop>
-          {ddata.map((item) => (
-            <CbtTopsp ref="dd"> {item.name}</CbtTopsp>
+          {ddata.map((item, index) => (
+            <CbtTopsp
+              ref={(el) => (itemRefs.current[index] = el)}
+              key={item.id}
+            >
+              {" "}
+              {item.name}
+            </CbtTopsp>
           ))}
         </CbtTop>
         <Cbtdiv>
@@ -95,6 +97,7 @@ function Customer() {
 }
 
 export default Customer;
+
 const Ctoprtspan = styled.span`
   color: black;
 `;
@@ -130,7 +133,6 @@ const Cbtdiv = styled.div`
   display: flex;
   flex-direction: column;
 `;
-
 const Combtn = styled.span`
   font-size: 40px;
 `;
@@ -155,7 +157,6 @@ const Topbtn = styled.button`
 const Bt = styled.div`
   display: flex;
   justify-content: space-between;
-
   align-items: center;
 `;
 const Btbtn = styled.button`
@@ -205,7 +206,6 @@ const Ctoprt = styled.div`
     color: skyblue;
   }
 `;
-
 const Cbt = styled.div`
   display: flex;
   align-items: center;
@@ -217,7 +217,7 @@ const CbtTop = styled.div`
   display: flex;
   margin: 50px 0px 50px 0px;
 `;
-const CbtTopsp = styled(Link)`
+const CbtTopsp = styled.span`
   background-color: rgb(240, 240, 240);
   border-radius: 25px;
   padding: 10px;
