1507e3a4536c54b01bac35361948bc23e74c2ea4
gusdn010158 <gusdn010158@gmail.com>
2024-08-05 21:07:05 +0900

    eedq

--- src/app/SignUp/page.js
+++ src/app/SignUp/page.js
@@ -3,7 +3,7 @@ import styles from "./signup.module.css";
 import RootLayout from "../layout";
 import Link from "next/link";
 
-function page() {
+function SignupPage() {
   return (
     <RootLayout withHeaderAndFooter={false}>
       <a href="/">
@@ -11,11 +11,10 @@ function page() {
           className={styles.signuptoplogo}
           alt="logo"
           src="https://ohstory.io/wp-content/uploads/2022/11/1_-Ohouse_mark_horizontal_black.png"
-        ></img>
+        />
       </a>
       <div className={styles.signup}>
         <div className={styles.signuplogo}>
-          {" "}
           <h3>회원가입</h3>
         </div>
         <div className={styles.signuptop}>
@@ -25,7 +24,6 @@ function page() {
               alt="1"
               src="https://blog.kakaocdn.net/dn/UGS0q/btree5Viurw/l07AH1VgWJHm4stsAHLdL0/img.png"
             />
-
             <img
               alt="2"
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/KakaoTalk_logo.svg/2048px-KakaoTalk_logo.svg.png"
@@ -38,9 +36,8 @@ function page() {
         </div>
         <div className={styles.signupmid}>
           <h4>이메일</h4>
-          <from>
-            <input className={styles.signupinput1} placeholder="이메일"></input>
-            @
+          <form>
+            <input className={styles.signupinput1} placeholder="이메일" />@
             <select className={styles.signupinput2}>
               <option>선택해주세요.</option>
               <option value="banana">바나나</option>
@@ -48,41 +45,33 @@ function page() {
               <option value="orange">오렌지</option>
             </select>
             <button className={styles.signupbutton1}>이메일 인증하기</button>
-          </from>
-
-          <from>
+          </form>
+          <form>
             <h4>비밀번호</h4>
             <div className={styles.signuptxt}>
               영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.
             </div>
-            <input
-              className={styles.signupinput}
-              placeholder="비밀번호"
-            ></input>
+            <input className={styles.signupinput} placeholder="비밀번호" />
             <h4>비밀번호 확인</h4>
-            <input
-              className={styles.signupinput}
-              placeholder="비밀번호 확인"
-            ></input>
+            <input className={styles.signupinput} placeholder="비밀번호 확인" />
             <h4>닉네임</h4>
             <div className={styles.signuptxt}>
               다른 유저와 겹치지 않도록 입력해주세요. (2~20자)
             </div>
-            <input className={styles.signupinput} placeholder="닉네임"></input>
+            <input className={styles.signupinput} placeholder="닉네임" />
             <h4>약관동의</h4>
-
             <button className={styles.signupbutton}>회원가입하기</button>
             <div className={styles.signupbt}>
               <span>이미 아이디가 있으신가요?</span>
-              <Link href="/Login" style={{ color: "black" }}>
+              <Link href="/Login" className={styles.loginLink}>
                 로그인
               </Link>
             </div>
-          </from>
+          </form>
         </div>
       </div>
     </RootLayout>
   );
 }
 
-export default page;
+export default SignupPage;
