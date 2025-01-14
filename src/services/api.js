d3cae4e6908d79ef83353852e5e29e2563386a0f
gusdn010158 <gusdn010158@gmail.com>
2025-01-15 01:24:06 +0900

    nextvercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,3 @@
-// "use client";
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-
-// const api = (url) => {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     axios.get(url).then((res) => {
-//       setData(res.data);
-//     });
-//   }, [url]);
-
-//   return data;
-// };
-
-// export default api;
 "use client";
 import { useState, useEffect } from "react";
 import axios from "axios";
